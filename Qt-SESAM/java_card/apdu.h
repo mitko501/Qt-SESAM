21fcb061458e0bedd0aa5bf349466cb43381a265
Andrej Staruch <s.andrej.002@gmail.com>
2017-05-01 17:26:39 +0200

    Added documentation.

--- Qt-SESAM/java_card/apdu.h
+++ Qt-SESAM/java_card/apdu.h
@@ -25,75 +25,138 @@ public:
   /**
    * @brief APDU Create clean APDU with applet class
    */
-  APDU() : APDU(0xB0, 0x00, 0x00, 0x00, 0x00, NULL) {}
-
+  APDU(): APDU(0xB0, 0x00, 0x00, 0x00, 0x00, NULL) {}
   /**
    * @brief APDU Create APDU with class and data
    * @param data_size
    * @param data
    */
-  APDU(byte data_size, const byte* data) : APDU(0xB0, 0x00, 0x00, 0x00, data_size, data) {} // Create APDU with data
-
+  APDU(byte data_size, const byte* data):
+      APDU(0xB0, 0x00, 0x00, 0x00, data_size, data) {}
+  /**
+   * @brief APDU
+   * @param ins instruction to perform on applet
+   */
   APDU(byte ins) : APDU(0xB0, ins, 0x00, 0x00, 0x00, NULL) {}
-
+  /**
+   * @brief APDU sets data w.r.t. to parameters
+   * @param aclass instruction class
+   * @param ins instruction code
+   * @param p1 instruction paramater
+   * @param p2 instruction parameter
+   * @param data_size size of the data
+   * @param data data
+   */
   APDU(byte aclass, byte ins, byte p1, byte p2, byte data_size, const byte* data);
-
+  /**
+   * @brief set_class set class of APDU command
+   * @param aclass class to set
+   */
   void set_class(byte aclass) {
     _data[OFFSET_CLA] = aclass;
   }
-
+  /**
+   * @brief get_class
+   * @return code of class
+   */
   byte get_class() {
     return _data[OFFSET_CLA];
   }
-
+  /**
+   * @brief set_ins set instruction of APDU command
+   * @param ins instruction to set
+   */
   void set_ins(byte ins) {
     _data[OFFSET_INS] = ins;
   }
-
+  /**
+   * @brief get_ins get code of instruction
+   * @return code of instruction
+   */
   byte get_ins() {
     return _data[OFFSET_INS];
   }
-
+  /**
+   * @brief set_p1 sets instruction parameter 1
+   * @param p1 parameter 1
+   */
   void set_p1(byte p1) {
     _data[OFFSET_P1] = p1;
   }
-
+  /**
+   * @brief get_p1 get instruction parameter 1
+   * @return instruction parameter 1
+   */
   byte get_p1() {
     return _data[OFFSET_P1];
   }
-
+  /**
+   * @brief set_p2 set instruction parameter 2
+   * @param p2 parameter 2
+   */
   void set_p2(byte p2) {
     _data[OFFSET_P2]= p2;
   }
-
+  /**
+   * @brief get_p2 get instruction parameter 2
+   * @return instruction parameter 2
+   */
   byte get_p2() {
     return _data[OFFSET_P2];
   }
-
+  /**
+   * @brief getDataPtr get pointer on start of data section
+   * @return pointer on start of data section
+   */
   byte* getDataPtr() {
     return _data + OFFSET_DATA;
   }
-
+  /**
+   * @brief set_data set data from byte buffer and forget about previous one
+   * @param size size of the data
+   * @param data
+   */
   void set_data(byte size, const byte* data);
-
+  /**
+   * @brief add_data append data to previous one
+   * @param size
+   * @param data
+   */
   void add_data(byte size, const byte* data);
-
+  /**
+   * @brief add_data append data from crypto integer
+   * @param data data to append
+   */
   void add_data(const CryptoPP::Integer data);
-
+  /**
+   * @brief add_data append data from byte block
+   * @param data data to append
+   */
   void add_data(const CryptoPP::SecByteBlock data);
-
+  /**
+   * @brief size
+   * @return size of data buffer plus header size
+   */
   byte size() const {
     return _data[OFFSET_LC] + HEADER_SIZE;
   }
-
+  /**
+   * @brief dataSize
+   * @return size of the data block
+   */
   byte dataSize() const {
     return _data[OFFSET_LC];
   }
-
+  /**
+   * @brief getRawAPDU
+   * @return raw data bytes
+   */
   byte* getRawAPDU() const {
     return _data;
   }
-
+  /**
+   * @brief APDU remove byte data block and free up the memory
+   */
   ~APDU();
 };
 
--- Qt-SESAM/java_card/securechannel.h
+++ Qt-SESAM/java_card/securechannel.h
@@ -12,33 +12,44 @@
 
 class SecureChannel
 {
-  CryptoPP::Integer _modulus;
-  CryptoPP::Integer _order;
-  CryptoPP::Integer _generator;
+private:
+  CryptoPP::Integer _modulus; //< Hardcoded modulus of DH (due to performance)
+  CryptoPP::Integer _order; //< Hardcoded order of DH (due to performance)
+  CryptoPP::Integer _generator; //< Generator of DH
 
-  CryptoPP::SecByteBlock _privatePart;
-  CryptoPP::SecByteBlock _publicPart;
-  CryptoPP::SecByteBlock _sessionKey;
+  CryptoPP::SecByteBlock _privatePart; //< Private key
+  CryptoPP::SecByteBlock _publicPart; //< Public key
+  CryptoPP::SecByteBlock _sessionKey; //< Session key
 
-  CryptoPP::AES::Decryption* _aesDecryption = NULL;
-  CryptoPP::AES::Encryption* _aesEncryption = NULL;
-  CryptoPP::CBC_Mode_ExternalCipher::Decryption _decryptor;
-  CryptoPP::CBC_Mode_ExternalCipher::Encryption _encryptor;
+  CryptoPP::AES::Decryption* _aesDecryption = NULL; //< Decryption function with session key
+  CryptoPP::AES::Encryption* _aesEncryption = NULL; //< Encryption function with session key
+  CryptoPP::CBC_Mode_ExternalCipher::Decryption _decryptor; //< Decryption in CBC Mode
+  CryptoPP::CBC_Mode_ExternalCipher::Encryption _encryptor; //< Encryption in CBC Mode
 
-  SCUtils* _smartCard;
+  SCUtils* _smartCard; //< Connected smart card
 
-  byte _iv[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  byte _iv[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; //< Initializing vector
 
 public:
+  /**
+   * @brief SecureChannel establish secure channel for exchanging password
+   * between application and smartcard. Protocol is used Diffie-Hellman.
+   * @param sc connected smartcard
+   */
   SecureChannel(SCUtils* sc);
-
+  /**
+   * @brief sendToCardSecurely encrypt apdu by session key and send it to card
+   * @param apdu apdu to encrypy
+   * @return response buffer
+   */
   APDUResponse sendToCardSecurely(APDU* apdu);
-
   void encrypt(byte* src, int srcSize, byte** dest, int* destSize);
   void encrypt(byte* src, int srcSize, CryptoPP::SecByteBlock* dest);
   void decrypt(byte* src, int srcSize, byte** dest, int* destSize);
   void decrypt(byte* src, int srcSize, CryptoPP::SecByteBlock* dest);
-
+  /**
+   * @brief SecureChannel defaul destructor. Free up memory.
+   */
   ~SecureChannel() {
     delete _aesDecryption;
     delete _aesEncryption;
